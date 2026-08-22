e4411a1f26e12c448cb3eb5226ab76e3ce93f950
Viktor Vaczi <viktor.vaczi@emergence-engineering.com>
2026-08-22 19:02:17 +0200

    test(collab): specify atomic native items batches

---
+++ wasm/bindings/tests/collab_items_validation_test.cpp
@@ -0,0 +1,43 @@
+#include "../collab_items_validation.h"
+
+#include <cassert>
+#include <string>
+#include <vector>
+
+using nlohmann::json;
+
+int main()
+{
+    std::string error;
+
+    const json valid = {
+        { "added", json::array( { { { "sexpr", "(segment (uuid a))" },
+                                      { "parent", nullptr } } } ) },
+        { "changed", json::array() },
+        { "removed", json::array( { "b" } ) },
+        { "_pcbjam", { { "requestId", "r1" }, { "ownerGeneration", "o1" } } },
+    };
+
+    assert( pcbjam_collab::validateItemsWireShape( valid, error ) );
+
+    json wrongCategory = valid;
+    wrongCategory["removed"] = "b";
+    assert( !pcbjam_collab::validateItemsWireShape( wrongCategory, error ) );
+
+    json blankBlob = valid;
+    blankBlob["added"][0]["sexpr"] = "  \n\t";
+    assert( !pcbjam_collab::validateItemsWireShape( blankBlob, error ) );
+
+    json wrongParent = valid;
+    wrongParent["added"][0]["parent"] = 42;
+    assert( !pcbjam_collab::validateItemsWireShape( wrongParent, error ) );
+
+    assert( pcbjam_collab::validateItemsBatchIds( { "removed" }, { "added" },
+                                                  { "changed" }, error ) );
+    assert( !pcbjam_collab::validateItemsBatchIds( { "same" }, { "same" }, {}, error ) );
+    assert( !pcbjam_collab::validateItemsBatchIds( {}, { "same", "same" }, {}, error ) );
+    assert( !pcbjam_collab::validateItemsBatchIds( {}, { "same" }, { "same" }, error ) );
+    assert( !pcbjam_collab::validateItemsBatchIds( { "" }, {}, {}, error ) );
+
+    return 0;
+}
